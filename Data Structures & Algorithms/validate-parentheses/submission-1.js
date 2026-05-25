8ee40c211c06516a7b5a6ced242075bdef24c458
Vishal Bisht <v.bisht9811@gmail.com>
2026-05-25 21:18:21 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        if (s.length % 2 !== 0) return false
+        const bracketMap = {
+            '[': ']',
+            '{': '}',
+            '(': ')'
+        }
+        const startBracket = Object.keys(bracketMap);
+        let bracketStack = [];
+        for (let i = 0; i < s.length; i++) {
+            if (startBracket.includes(s[i])) {
+                bracketStack.push(s[i]);
+            } else {
+            console.log(bracketStack,s[i])
+                if (bracketMap[bracketStack.pop()] !== s[i]) {
+                    return false;
+                }
+            }
+        }
+        return bracketStack.length === 0 
+    }
+}
